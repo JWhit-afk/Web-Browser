e4f802ccd025b96c9c596d67a85c2b091a58fef6
JWhit-afk <Jacobwhitbourn@gmail.com>
2025-12-04 19:18:16 +0000

    feat: HTTP Client Implements a wrapper around HttpClient to simplify usage for scope

---
+++ HTTPClient.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Web_Browser_CW1 {
+
+    struct HttpResponse {
+
+        public string title;
+        public Icon? favicon;
+        public string body;
+        public HttpStatusCode statusCode;
+
+        public HttpResponse(string title, Icon favicon, string body, HttpStatusCode statusCode) {
+            this.title = title;
+            this.favicon = favicon;
+            this.body = body;
+            this.statusCode = statusCode;
+        }
+        public HttpResponse(string title, string body, HttpStatusCode statusCode) {
+            this.title = title;
+            this.body = body;
+            this.statusCode = statusCode;
+            this.favicon = null;
+        }
+
+    }
+
+    internal class HTTPClient {
+
+        private ProgressBar loadIndicator;
+
+        private static readonly HttpClient client = new HttpClient();
+
+        public HTTPClient(ProgressBar loadIndicator) { this.loadIndicator = loadIndicator; }
+
+        public async Task<HttpResponse> Get(string url) {
+
+            loadIndicator.Show();
+
+            try {
+
+                // submit HTTP request
+                using HttpResponseMessage response = await client.GetAsync(url);
+                String responseBody = await response.Content.ReadAsStringAsync();
+                HttpStatusCode statusCode = response.StatusCode;
+
+                // Get title of page
+                Regex reg = new Regex("<title>(.*)</title>");
+                MatchCollection match = reg.Matches(responseBody);
+                string title = "website";
+
+                if (match.Count == 1) {
+                    title = match.First().Value;
+                    title = title.Substring(7);
+                    title = title.Substring(0, title.Length - 8);
+                }
+
+                // Get favicon
+                Bitmap faviconBitmap = new Bitmap(await client.GetStreamAsync(url + "/favicon.ico"));
+                nint handle = faviconBitmap.GetHicon();
+                Icon favicon = Icon.FromHandle(handle);
+
+                return new HttpResponse(title, favicon, responseBody, statusCode);
+
+            } catch (Exception ex) {
+
+                Console.WriteLine(ex.Message);
+                return new HttpResponse("website", "Unknown error please try again later", 0);
+            
+            } finally {
+
+                loadIndicator.Hide();
+            }
+        }
+    }
+}
--- WebBrowser.cs
+++ WebBrowser.cs
@@ -5,10 +5,31 @@ namespace Web_Browser_CW1
 {
     public partial class WebBrowser : Form {
 
+        HTTPClient httpClient;
+
         public WebBrowser() {
 
             InitializeComponent();
 
+            httpClient = new HTTPClient(progressBar);
+
+            progressBar.Hide();
+
+        }
+
+        private void RenderHtmlCode(HttpResponse response) {
+
+            // display html + status code
+            this.Text = response.title;
+
+            if (response.favicon != null) {
+                this.Icon = response.favicon;
+            } else {
+                this.Icon = null;
+            }
+
+            htmlDisplay.Text = response.body;
+            HtmlResponseCodeOutput.Text = "Response Code: " + ((int)response.statusCode) + " - " + response.statusCode.ToString();
         }
 
         private void WebBrowserWindow_Load(object sender, EventArgs e) {
